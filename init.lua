bd29f8518efbc1ec81356435ece2fe2c0c7f377b
Nitesh Kumar Debnath <30214255+veechiecooqu@users.noreply.github.com>
2023-07-13 12:10:16 +0530

    Astromars again

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "tokyonight-moon",
+  colorscheme = "astromars",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
